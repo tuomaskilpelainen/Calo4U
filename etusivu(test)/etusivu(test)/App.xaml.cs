9f9ed971b86aded48128f78beec7d3e7a5107d84
bunyisaboni <kuu.libns@msn.com>
2024-02-14 12:13:00 +0200

    etusivun testi

---
+++ etusivu(test)/etusivu(test)/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace etusivu_test_
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}

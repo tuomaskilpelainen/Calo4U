5e1e28f6de4709dfa087f6dbab3fe2fdcd737653
bunyisaboni <kuu.libns@msn.com>
2024-02-17 12:40:17 +0200

    Wpf-pohjat lisätty

---
+++ etusivuWpf/etusivu(test2)/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace etusivu_test2_
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}

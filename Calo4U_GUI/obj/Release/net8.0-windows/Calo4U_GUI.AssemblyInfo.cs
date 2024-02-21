e2009dd03fb50cebf7400771cc54438a5045d360
bunyisaboni <kuu.libns@msn.com>
2024-02-21 12:28:43 +0200

    KatsoResepti -sivun WPF pohja on nyt lisätty

--- Calo4U_GUI/obj/Release/net8.0-windows/Calo4U_GUI.AssemblyInfo.cs
+++ Calo4U_GUI/obj/Release/net8.0-windows/Calo4U_GUI.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Calo4U_GUI")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d133db6047f321ba93fff176b8d35f4069db5ec6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Calo4U_GUI")]

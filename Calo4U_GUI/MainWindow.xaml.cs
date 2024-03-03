1b668a8167d2dbce841d19bda0615270d0e8afed
tuomaskilpelainen <tuomas1818@student.hamk.fi>
2024-03-03 07:37:55 +0200

    Lisätty jotenkin toimiva piechart MainWindowiin

--- Calo4U_GUI/MainWindow.xaml.cs
+++ Calo4U_GUI/MainWindow.xaml.cs
@@ -9,6 +9,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Calo4U_Sisa;
+using static System.Net.Mime.MediaTypeNames;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 
 namespace Calo4U_GUI
@@ -18,9 +20,17 @@ namespace Calo4U_GUI
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        int CaloriesEaten = 13000;
+        int GoalCalories = 20000;
+        int LastCaloriesEaten = 17000;
+        int RemainingCalories = 0;
+        int PieChartCalories = 0;
         public MainWindow()
         {
             InitializeComponent();
+            PieChartCalories = CaloriesEaten;
+            PieChart();
         }
 
         private void etusivuNavButton_Click(object sender, RoutedEventArgs e)
@@ -48,6 +58,55 @@ namespace Calo4U_GUI
         public void ShowMainWindow()
         {
             MainFrame.Visibility = Visibility.Collapsed;
+        }
+
+        private void PieChart()
+        {
+            RemainingCalories = GoalCalories - PieChartCalories;
+            saavMääräNroBlock.Text = PieChartCalories.ToString();
+            calJälBlock.Text = RemainingCalories.ToString();
+            tavoiteNroBlock.Text = GoalCalories.ToString();
+            int RemainingCalories1 = 0;
+            RemainingCalories1 = RemainingCalories;
+            if (RemainingCalories1 < 0)
+            {
+                RemainingCalories1 = 0;
+            }
+            pieChart.Series = new LiveCharts.SeriesCollection
+            {
+                new LiveCharts.Wpf.PieSeries
+                {
+                    Title = "Viikon syödyt kalorit",
+                    Values = new LiveCharts.ChartValues<int> { PieChartCalories },
+                    DataLabels = true,
+                    Fill = new SolidColorBrush(Color.FromRgb(46, 51, 78))
+                },
+                new LiveCharts.Wpf.PieSeries
+                {
+                    Title = "Viikon Jäljellä olevat kalorit",
+                    Values = new LiveCharts.ChartValues<int> { RemainingCalories1 },
+                    DataLabels = true,
+                    Fill = new SolidColorBrush(Color.FromRgb(255, 138, 81))
+                }
+            };
+        }
+
+        private void TextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox textBox = (TextBox)sender;
+            if (textBox.Text == "Manuaalinen syöttö")
+            {
+                textBox.Text = "";
+            }
+        }
+
+        private void TextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox textBox = (TextBox)sender;
+            if (string.IsNullOrWhiteSpace(textBox.Text))
+            {
+                textBox.Text = "Manuaalinen syöttö";
+            }
         }
     }
 }
